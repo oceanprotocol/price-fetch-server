37622d7ab1a7fd0f393f3bc1ca285c6359d5e9d9
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-04-14 14:35:49 +0300

    Creating route and adding package-lock.json

---
+++ routes/index.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+import express from 'express'
+
+const router = express.Router()
+
+/* GET Access role premissions. */
+router.get(' /ipfs/QmTkzDwWqPbnAh5YiV5VwcTLnGdwSNsNTn2aDxdXBFca7D/', function (req: Request, res: Response) {
+  // Request Price
+})
+
+export default router
