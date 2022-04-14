d712776ddc097240c2d91136896eb269dcd1cc8f
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-04-14 14:40:15 +0300

    Adding controller

---
+++ controllers/accessController.ts
@@ -0,0 +1,12 @@
+import { Response } from 'express'
+
+async function accessController(tokenSymbol: string, timestamp: string, res: Response) {
+  console.log('tokenSymbol', tokenSymbol)
+  console.log('timestamp', timestamp)
+  let price
+  // request price
+  price = 100
+  res.send(price)
+}
+
+export default accessController
