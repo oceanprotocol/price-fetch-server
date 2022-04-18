ee460ace8629d6db6d55dfeb3b5487f89211a476
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-04-18 14:10:38 +0300

    Saving prices in cache

--- src/controllers/getHistoricalPrice.ts
+++ src/controllers/getHistoricalPrice.ts
@@ -1,17 +1,32 @@
 import { Request, Response } from 'express'
 import fetch from 'cross-fetch'
+import cache from 'memory-cache'
 
-async function historicalPrice(req: Request, res: Response) {
+async function historicalPrice(tokenId: string, date: string) {
   try {
-    const { tokenId, date } = req.params
     const url = `https://api.coingecko.com/api/v3/coins/${tokenId}/history?date=${date}`
     const response = await fetch(url)
     const body = await response.json()
-    res.send({ usd: body.market_data.current_price.usd })
+    const price = { usd: body.market_data.current_price.usd }
+    cache.put(`${tokenId}-${date}`, price)
+    return price
+  } catch (error) {
+    console.error('ERROR:', error)
+  }
+}
+
+async function getHistoricalPrice(req: Request, res: Response) {
+  try {
+    const { tokenId, date } = req.params
+    let price = cache.get(`${tokenId}-${date}`)
+    if (price === null) {
+      price = await historicalPrice(tokenId, date)
+    }
+    res.send(price)
   } catch (error) {
     console.error('ERROR:', error)
     res.send(error)
   }
 }
 
-export default historicalPrice
+export default getHistoricalPrice
